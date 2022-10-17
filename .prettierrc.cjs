6d60e5547098cc05910a48f8756c1a3228017323
ioofy <mynev.id@gmail.com>
2022-10-17 13:10:17 +0700

    feat: setup app and config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,10 +1,9 @@
 module.exports = {
-    printWidth: 120,
-    semi: true,
-    singleQuote: false,
-    trailingComma: "all",
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    arrowParens: "avoid",
-  };
-  
+  printWidth: 120,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+};
