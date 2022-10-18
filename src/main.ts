7ecef346c41410d6fc3015e5470bd7281bc39c77
ioofy <mynev.id@gmail.com>
2022-10-18 12:40:28 +0800

    feat: adding new transform

--- src/main.ts
+++ src/main.ts
@@ -16,16 +16,38 @@ const transformToCamelCase = <T extends object>(obj: T): CamelCasedPropertiesDee
   return result as CamelCasedPropertiesDeep<T>;
 };
 
-const transformToSnakeCase = <T extends object>(obj: T): KebabCasedPropertiesDeep<T> => {
+const transformToKebabCase = <T extends object>(obj: T): KebabCasedPropertiesDeep<T> => {
+  const result: T = _.transform<T, any>(obj, (acc, value, key, target) => {
+    const camelKey = _.isArray(target) ? key : _.kebabCase(key as string);
+
+    acc[camelKey] = _.isObject(value) ? transformToKebabCase(value) : value;
+  });
+  return result as KebabCasedPropertiesDeep<T>;
+};
+
+const transformToSnakeCase = <T extends object>(obj: T): SnakeCasedPropertiesDeep<T> => {
   const result: T = _.transform<T, any>(obj, (acc, value, key, target) => {
     const camelKey = _.isArray(target) ? key : _.snakeCase(key as string);
 
     acc[camelKey] = _.isObject(value) ? transformToSnakeCase(value) : value;
   });
-  return result as KebabCasedPropertiesDeep<T>;
+  return result as SnakeCasedPropertiesDeep<T>;
 };
+
+const transformToPascalCase = <T extends object>(obj: T): PascalCasedPropertiesDeep<T> => {
+  const result: T = _.transform<T, any>(obj, (acc, value, key, target) => {
+    const camelKey = _.isArray(target) ? key : _.upperCase(key as string);
+
+    acc[camelKey] = _.isObject(value) ? transformToPascalCase(value) : value;
+  });
+  return result as PascalCasedPropertiesDeep<T>;
+};
+
+export { transformToSnakeCase, transformToKebabCase, transformToCamelCase, transformToPascalCase };
 
 export default {
   transformToSnakeCase,
+  transformToKebabCase,
   transformToCamelCase,
+  transformToPascalCase,
 };
