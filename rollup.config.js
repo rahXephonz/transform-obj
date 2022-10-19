51718b5aaeea86792f0b1ff2989b4aaba77e5725
ioofy <mynev.id@gmail.com>
2022-10-19 22:49:59 +0700

    chore(pkg): release to v1.0.2

--- rollup.config.js
+++ rollup.config.js
@@ -22,7 +22,7 @@ export default [
     ],
 
     preserveModules: true,
-    external: ["lodash"],
+    external: [],
   },
   {
     input: ["src/main.ts"],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import _ from "lodash";
 import * as typeFest from "type-fest";
 
-const transformToCamelCase = <T extends object>(obj: T): typeFest.CamelCasedPropertiesDeep<T> => {
+export const transformToCamelCase = <T extends object>(obj: T): typeFest.CamelCasedPropertiesDeep<T> => {
   const result: T = _.transform<T, any>(obj, (acc, value, key, target) => {
     const camelKey = _.isArray(target) ? key : _.camelCase(key as string);
 
@@ -11,7 +11,7 @@ const transformToCamelCase = <T extends object>(obj: T): typeFest.CamelCasedProp
   return result as typeFest.CamelCasedPropertiesDeep<T>;
 };
 
-const transformToKebabCase = <T extends object>(obj: T): typeFest.KebabCasedPropertiesDeep<T> => {
+export const transformToKebabCase = <T extends object>(obj: T): typeFest.KebabCasedPropertiesDeep<T> => {
   const result: T = _.transform<T, any>(obj, (acc, value, key, target) => {
     const camelKey = _.isArray(target) ? key : _.kebabCase(key as string);
 
@@ -20,7 +20,7 @@ const transformToKebabCase = <T extends object>(obj: T): typeFest.KebabCasedProp
   return result as typeFest.KebabCasedPropertiesDeep<T>;
 };
 
-const transformToSnakeCase = <T extends object>(obj: T): typeFest.SnakeCasedPropertiesDeep<T> => {
+export const transformToSnakeCase = <T extends object>(obj: T): typeFest.SnakeCasedPropertiesDeep<T> => {
   const result: T = _.transform<T, any>(obj, (acc, value, key, target) => {
     const camelKey = _.isArray(target) ? key : _.snakeCase(key as string);
 
@@ -29,20 +29,11 @@ const transformToSnakeCase = <T extends object>(obj: T): typeFest.SnakeCasedProp
   return result as typeFest.SnakeCasedPropertiesDeep<T>;
 };
 
-const transformToPascalCase = <T extends object>(obj: T): typeFest.PascalCasedPropertiesDeep<T> => {
+export const transformToPascalCase = <T extends object>(obj: T): typeFest.PascalCasedPropertiesDeep<T> => {
   const result: T = _.transform<T, any>(obj, (acc, value, key, target) => {
     const camelKey = _.isArray(target) ? key : _.upperCase(key as string).replace(/ /g, "_");
 
     acc[camelKey] = _.isObject(value) ? transformToPascalCase(value) : value;
   });
   return result as typeFest.PascalCasedPropertiesDeep<T>;
-};
-
-export { transformToSnakeCase, transformToKebabCase, transformToCamelCase, transformToPascalCase };
-
-export default {
-  transformToSnakeCase,
-  transformToKebabCase,
-  transformToCamelCase,
-  transformToPascalCase,
 };
